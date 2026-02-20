16c44b0f081ad46ae6b1d56adb9622934842c371
shreyajare2007-cloud <shreyajare2007@gmail.com>
2026-02-20 13:19:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,9 @@
 #      User enters name: eg: Shibu. and the program should print "Hello, Shibu" 
 # Coder: 
 # Date:
-
+name=input("lord")
+age=input("0")
+print(f"""Welcome to Rizvi College of Engineering
+Hello, {name}
+You are {age} years old.""")
 # Write your code here
